19f8a082ab29336466d241639f1bd03092bc4ffd
yevhenboiev@gmail.com <zipzone12345>
2021-12-01 14:02:02 +0300

    add annotation Repository

--- src/main/java/ru/simbirsoft/chat/repository/RoomRepository.java
+++ src/main/java/ru/simbirsoft/chat/repository/RoomRepository.java
@@ -1,7 +1,9 @@
 package ru.simbirsoft.chat.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.simbirsoft.chat.entity.Room;
 
+@Repository
 public interface RoomRepository extends JpaRepository<Room, Long> {
 }
