4df6dfe018965ae170c0eed696adb5f4b009fb07
yevhenboiev <yevhenboiev@gmail.com>
2022-01-16 15:32:45 +0300

    add formatter time

--- src/main/java/ru/simbirsoft/chat/dto/ClientDto.java
+++ src/main/java/ru/simbirsoft/chat/dto/ClientDto.java
@@ -8,7 +8,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import ru.simbirsoft.chat.entity.enums.Role;
 
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.sql.Timestamp;
 import java.util.Set;
--- src/main/java/ru/simbirsoft/chat/dto/RoomDto.java
+++ src/main/java/ru/simbirsoft/chat/dto/RoomDto.java
@@ -7,7 +7,6 @@ import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import java.util.List;
 import java.util.Set;
 
 @Setter
