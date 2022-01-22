cfddd2b00653b28a1e2cb97a0d3674a6320c30e4
yevhenboiev <yevhenboiev@gmail.com>
2022-01-22 12:53:25 +0300

    create DB Migration

--- src/main/java/ru/simbirsoft/chat/Application.java
+++ src/main/java/ru/simbirsoft/chat/Application.java
@@ -1,33 +1,12 @@
 package ru.simbirsoft.chat;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import ru.simbirsoft.chat.entity.Client;
-import ru.simbirsoft.chat.repository.ClientRepository;
 
 @SpringBootApplication
-public class Application implements CommandLineRunner {
-
-    private final ClientRepository clientRepository;
-
-    @Autowired
-    public Application(ClientRepository clientRepository) {
-        this.clientRepository = clientRepository;
-    }
+public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        Client client = new Client();
-        client.setId(1L);
-        client.setLogin("Bot");
-        client.setPassword("Bot");
-        client.setActive(true);
-        clientRepository.save(client);
     }
 }
---
+++ src/main/resources/db/migration/V2__Create_Client.sql
@@ -0,0 +1,5 @@
+insert into client (id, end_ban, is_active, is_block, login, password, role, start_ban)
+    values (1, null, true, false, 'Bot', 'Bot', null, null);
+
+insert into client (id, end_ban, is_active, is_block, login, password, role, start_ban)
+    values (2, null, true, false, 'Admin', '$2a$12$jQADut2UBqFEZqrhgjajpehc8xeXdSBGhyd3G0ju5.1xFvYu1kX5m', 'ADMIN', null);
